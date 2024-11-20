928b1a6254e14da3946278c012f3faac948b51b5
apolloalves <apolloapolloalves@gmail.com>
2024-11-20 12:13:57 -0300

    Seu commit

--- installers/carbo__InstallCarbonara.sh
+++ installers/carbo__InstallCarbonara.sh
@@ -22,38 +22,53 @@ fi
 # (sudo) privileges for certain operations.                                        #
 #                                                                                  #
 ####################################################################################
-
+clear
 # Defines the root directory from which find will be run
 ROOT_DIR="/"
 echo -e "\n\033[01;37m\033[01mwait...\033[00;37m\033[00m\n"
 
-# Use the find command to locate the "LinuxBashCare" folder
-path=$(find "$ROOT_DIR" -type d -name "Carbonara" 2>/dev/null)
-
+# Use the find command to locate the "carbonara" folder
+path=$(find "$ROOT_DIR" -type d -name "carbonara" 2>/dev/null)
+rm -rfv /home/apollo/Projects/.gnome-builder/projects/carbonara 2>/dev/null
 # Checks if the folder was found
 
 if [ -n "$path" ]; then
 
     sleep 2
-    echo -e "\n\033[01;32mThe cloned 'LinuxBashCare' folder was found in: $path\033[00;37m\n"
+    echo -e "\n\033[01;32mThe cloned 'Carbonara' folder was found in: $path\033[00;37m\n"
 
-    ls -lsht $path/System/bin/*.sh
-    find $path/System/bin/ -ls | wc -l
+    ls -lsht $path
+    echo
+    echo "Lozalizado pastas"
+    find $path/ -ls | wc -l
+
+    echo "Lozalizado executaveis"
+    find $path/*/*.sh -ls | wc -l
+    echo
+    echo
+    ls -lsht $path/*/*.sh
+
+
+    sleep 5
 
     echo -e "\n\033[01;32msetting execute permissions on shell files...\033[00;37m"
     sleep 2
-    sudo chmod +x $path/System/bin/*.sh
+    ls -l $path/*/*.sh
+    echo "definindo proprietario"
+    sudo chown root:root carbo__*
+    sleep 3
+    echo "definindo permissões"
+    chmod +x $path/*/*.sh
+
     echo -e "\n\033[01;32mcopying the executables to the /bin folder...\033[00;37m\n"
     sleep 2
-    sudo rsync -avh --progress $path/System/bin/*.sh /bin/
+    sudo rsync -avh --progress $path/*/*.sh /bin/
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32mupdate packages of system...\033[00;37m\n"
     echo -e "\n\033[01;37m\033[01mwait...\033[00;37m\033[00m\n"
     sleep 2
-    sudo apt update -y && sudo apt upgrade -y
-    sudo dpkg --configure -a
-    sudo apt install -f
+    sudo pacman -Syu --noconfirm
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
     echo -e "\n\033[01;32minstalling dependencies...\033[00;37m\n"
     sleep 3
@@ -62,43 +77,33 @@ if [ -n "$path" ]; then
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling neofetch...\033[00;37m\n"
-    sudo apt install neofetch -y
+    sudo pacman -S neofetch --noconfirm
     echo ""
     neofetch
     echo ""
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
-    
-    echo -e "\n\033[01;32minstalling deborphan...\033[00;37m\n"
-    
-    sudo apt install deborphan -y
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling preload...\033[00;37m\n"
     sleep 2
-    sudo apt install preload
+    sudo pacman -S preload
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling prelink...\033[00;37m\n"
     sleep 2
-    sudo apt install prelink -y
-    sudo sed -i '6s/PRELINKING=.*/PRELINKING=yes/' /etc/default/prelink >/dev/null
-    sudo chmod 666 /etc/prelink.conf
-    sudo prelink -amvR
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    #sudo  install prelink -y
+    #sudo sed -i '6s/PRELINKING=.*/PRELINKING=yes/' /etc/default/prelink >/dev/null
+    #sudo chmod 666 /etc/prelink.conf
+    #sudo prelink -amvR
+    #echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling trash-cli...\033[00;37m\n"
     sleep 2
-    sudo apt-get install trash-cli
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
-
-    echo -e "\n\033[01;32minstalling stacer...\033[00;37m\n"
-    sleep 2
-    sudo apt install stacer -y
+    sudo pacman -S trash-cli
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling flatpak...\033[00;37m\n"
     sleep 2
-    sudo apt install flatpak -y
+    sudo pacman -S flatpak -y
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minserting history format in ~/.bashrc\033[00;37m\n"
@@ -106,13 +111,14 @@ if [ -n "$path" ]; then
     source ~/.bashrc
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
     echo
-    broot_install.sh  
+    carbo__BrootInstall.sh
     sleep 1
     echo -e "\n\033[01;32minstalling Penguins-eggs...\033[00;37m\n"
-    source /bin/eggs__install.sh
+    source /bin/carbo__EggsInstall.sh
     echo -e "\n\033[01;05;37mLinuxBashCare-10.12.23 successfully installed!!\033[00;37m\n"
     echo "Executando LinuxBashCare-10.12.23..."
     sleep 2 
-    LinuxBashCare-10.12.23.sh
+    carbonara.sh
 
 fi
+
--- installers/carbo__UnistallCarbonara.sh
+++ installers/carbo__UnistallCarbonara.sh
@@ -5,51 +5,61 @@ if (( EUID != 0 )); then
     echo "Please execute this with sudo."
     exit 1
 fi
-
+####################################################################################
+#                                                                                  #
+# Script: install.sh - LinuxBashCare Setup Script                                  #
+#                                                                                  #
+# Author: Apollo Alves                                                             #
+# Date: 16/12/2023                                                                 #
+#                                                                                  #
+# Description:                                                                     #
+# This script automates the setup process for LinuxBashCare. It performs           #
+# various tasks, including locating the LinuxBashCare folder, setting              #
+# execute permissions, copying executables to /bin, updating the system,           #
+# installing dependencies, and configuring system utilities.                       #
+#                                                                                  #
+# Note: This script assumes a Debian-based system and requires superuser           #
+# (sudo) privileges for certain operations.                                        #
+#                                                                                  #
+####################################################################################
+clear
 # Defines the root directory from which find will be run
 ROOT_DIR="/"
-CARBONARA_DIR="$HOME/Carbonara"
+BIN='/usr/bin/carbo__*'
 echo -e "\n\033[01;37m\033[01mwait...\033[00;37m\033[00m\n"
 
-# Use the find command to locate the "LinuxBashCare" folder
-path=$(find "$ROOT_DIR" -type d -name "Carbonara" 2>/dev/null)
+# Use the find command to locate the "carbonara" folder
+path=$(find "$ROOT_DIR" -type d -name "carbonara" 2>/dev/null)
+rm -rfv /home/apollo/Projects/.gnome-builder/projects/carbonara 2>/dev/null
+# Checks if the folder was found
 
-####################################################################################
-# Checks if the folder/files was found and remove
-####################################################################################
 
+
+# Checks if the folder was found
 if [ -n "$path" ]; then
 
     sleep 2
     echo -e "\n\033[01;32mThe cloned 'Carbonara' folder was found in: $path\033[00;37m\n"
 
+    ls -lsht $path
+    echo
+    echo "Lozalizado pastas"
+    find $path/ -ls | wc -l
+
+    echo "Lozalizado executaveis"
+    find $path/*/*.sh -ls | wc -l
+    echo
+    echo
     ls -lsht $path/*/*.sh
-    find $path/* -ls | wc -l
-
-    # sudo rm -rfv $path
-    echo -e "\n\033[01;32mRemoving files from Carbonara...\033[00;37m\n"
-    sleep 2
-    find '/bin/' -name "*__*.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "*_*.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "pacmanCleanCache.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "pacman-remove.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "pamac-manager.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "pacman-system.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "pacman-update.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "carbonara.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "report.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "scripts.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "ventoy.sh" -exec sudo rm -rfv {} \; | wc -l
-    find '/bin/' -name "menu.sh" -exec sudo rm -rfv {} \; | wc -l
-    sudo rm -rfv $HOME/plot
-    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"
-        
-
-    echo "Thank you!"
+    rm -rfv $path
+    rm -rfv $BIN
+    rm -rfv $BIN/carbonara.sh
+fi
+ echo "Thank you!"
     sleep 2 
     echo "bye!"
 
     exit
 
-
 fi
+i
