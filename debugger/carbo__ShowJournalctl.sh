14603666ecf22b1cb239b9e501c2aa9bc2fbd4d7
apolloalves <apolloapolloalves@gmail.com>
2024-11-21 10:37:39 -0300

    Fix text

--- debugger/carbo__ShowJournalctl.sh
+++ debugger/carbo__ShowJournalctl.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
+##################################################################################################################
+# Script: carbo__ShowJournalctl.sh                                                                               #
+# Author: Apollo Alves                                                                                           #
+# Date: 21/11/2024                                                                                               #
+#                                                                                                                #
+# Description:                                                                                                   #
+# This script provides options to view and monitor system logs using journalctl. It includes commands to view    #
+# failed boot messages, all system logs (including error messages during initialization), check service status,  #
+# and monitor syslog. The menu-driven interface simplifies the process of accessing and analyzing system logs.   #
+##################################################################################################################
+
 # Check if the user is root
 if ((EUID != 0)); then
     echo "This script needs to be run as root."
     echo "Please execute this with sudo."
     exit 1
 fi
-#####################################################################
-#                                                                   #
-# Script: show__journalctl.sh                                       #
-# Author: Apollo Alves                                              #
-# Date: 16/12/2023                                                  #
-#                                                                   #
-#####################################################################
-
-##################################################################################################################
-#                                                                                                                #
-# Description:                                                                                                   #
-# This script provides options to view and monitor system logs using journalctl. It includes commands to view    #
-# failed boot messages, all system logs (including error messages during initialization), check service status,  #
-# and monitor syslog. The menu-driven interface simplifies the process of accessing and analyzing system logs.   #
-#                                                                                                                #
-##################################################################################################################
 
 clear
 
@@ -51,6 +46,7 @@ echo -e "\033[01;32m[\033[01;37m 3\033[01;32m ]\033[00;37m - Check service statu
 echo -e "\033[01;32m[\033[01;37m 4\033[01;32m ]\033[00;37m - Syslog Monitor\033m"
 echo -e "\033[01;32m[\033[01;37m E\033[01;32m ]\033[00;37m - Exit\033m"
 echo
+
 $LINE_SCRIPT
 formatted_prompt=$(printf "\e[1;97mInput option :\e[0m ")
 read -p "$formatted_prompt" option_choice
@@ -78,32 +74,30 @@ if [ "$option_choice" != "e" ] && [ "$option_choice" != "E" ]; then
             $JORNALMENU
             ;;
 
-      4)
+        4)
 	   echo -e "\n\033[01;05;32mMapping system activities...\033[00;37m\n"
-	    echo -e "\033[1;97mPress Ctrl+C at any time to exit and return to the menu\033[0m"
-	    sleep 2
+	   echo -e "\033[1;97mPress Ctrl+C at any time to exit and return to the menu\033[0m"
+	   sleep 2
 
-	    # Captura Ctrl+C durante o monitoramento com journalctl -f
-	    trap ctrl_c INT
+           # Capture Ctrl+C while monitoring with journalctl -f
+	   trap ctrl_c INT
 
-	    # Função para lidar com Ctrl+C
-	    ctrl_c() {
+	   # Function to handle Ctrl+C
+	   ctrl_c() {
 		echo -e "\n\033[01;31mCtrl+C detected! Returning to the menu...\033[0m"
 		sleep 2
 		clear
 		$JORNALMENU
 	    }
 
-	    # Inicia o monitoramento com journalctl -f
+	    # Start monitoring with journalctl -f
 	    journalctl -f
 
-	    # Após o monitoramento, desativar o trap
+	    # After monitoring, disable the trap
 	    trap - INT
-
 	    clear
 	    $JORNALMENU
 	    ;;
-
 
         *)
             echo -e "\nInvalid input! Please enter the number contained in the menu!.\n"
